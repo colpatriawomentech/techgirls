9bab788ac780ab135735c2226506732980bd47bc
Milena Suarez <milena.suarez.luna@gmail.com>
2018-06-26 21:27:23 -0500

    add sass and boostrap 4.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
